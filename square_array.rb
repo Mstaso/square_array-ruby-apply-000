ab18a0af8ca79ca469059e0465f77f512edb0451
malcolm staso <mstaso1@gmail.com>
2020-04-21 21:03:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,17 +7,7 @@ def square_array(array)
   array.each do |integer|
     new_array << integer ** 2
  end
- return new_array
 end 
 
 
 
-def square_array(array)
-
- new_array = []
-
- array.each do |integer|
-  new_array << integer ** 2
- end
- return new_array
-end

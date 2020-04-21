84f4ccec530917790d4b1e2a62cb91084e768631
malcolm staso <mstaso1@gmail.com>
2020-04-21 20:42:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
-end
+numbers = [1,2,3]
+
+def square_array(numbers)
+  numbers.each do |numbers|
+    #{numbers} ** 2
+ end
+ square_array(numbers)
+end 
+
+

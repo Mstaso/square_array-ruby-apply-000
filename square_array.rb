41f2eb716e62d9f3d88d9fe3ba8c240e6022e764
malcolm staso <mstaso1@gmail.com>
2020-04-21 21:00:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,22 @@
-numbers = [1,2,3]
 
-def square_array(numbers)
+
+def square_array(array)
+  
+  numbers = [1,2,3]
+  
   numbers.each do |numbers|
   #{numbers} ** 2
  end
 end 
 
+def square_array(array)
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
+end
 
 
